48cbafcf1f527fd43d962894da5a474ef2585df6
Ivan Tomofeev <iv.timofeev@yandex.ru>
2017-10-16 12:45:50 +0800

    Сделан browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,21 @@
 var gulp = require('gulp');
 var sass = require("gulp-sass");
+var server = require('browser-sync');
+var plumber = require('gulp-plumber');
 
-gulp.task('sass', function () {
+gulp.task('style', function () {
   gulp.src('sass/style.scss')
+    .pipe(plumber())
     .pipe(sass())
-    .pipe(gulp.dest('css'));
+    .pipe(gulp.dest('css'))
+    .pipe(server.reload({ stream: true }));
 })
 
+gulp.task('server', ['style'], function () {
+  server.init({
+    server: "."
+  });
+
+  gulp.watch('sass/**/*.scss', ['style']);
+  gulp.watch('*.html').on('change',server.reload);
+});
