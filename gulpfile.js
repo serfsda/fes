c06e95f23f05c6f705d111b314b5df72b01952d3
Ivan Timofeev <iv.timofeev@yandex.ru>
2017-10-18 00:05:25 +0800

    Доработка автоматизации

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,28 @@ var gulp = require('gulp');
 var sass = require("gulp-sass");
 var server = require('browser-sync');
 var plumber = require('gulp-plumber');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var mqpacker = require('css-mqpacker');
 
 gulp.task('style', function () {
   gulp.src('sass/style.scss')
     .pipe(plumber())
     .pipe(sass())
+    .pipe(postcss([
+      autoprefixer({
+        browsers: [
+          "last 1 version",
+          "last 2 Chrome versions",
+          "last 2 Firefox versions",
+          "last 2 Opera versions",
+          "last 2 Edge versions",
+        ]
+      }),
+      mqpacker({
+        sort: true
+      })
+    ]))
     .pipe(gulp.dest('css'))
     .pipe(server.reload({ stream: true }));
 })
@@ -17,5 +34,5 @@ gulp.task('server', ['style'], function () {
   });
 
   gulp.watch('sass/**/*.scss', ['style']);
-  gulp.watch('*.html').on('change',server.reload);
+  gulp.watch('*.html').on('change', server.reload);
 });
