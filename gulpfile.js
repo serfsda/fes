cb9a1fa18b487b492313db9a60760deccbcda161
Ivan Tomofeev <iv.timofeev@yandex.ru>
2017-10-19 14:26:27 +0800

    Автоматизация закончена

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,8 @@ var mqpacker = require('css-mqpacker');
 var minify = require('gulp-csso');
 var rename = require('gulp-rename');
 var imagemin = require('gulp-imagemin');
+var run = require('run-sequence');
+var del = require('del');
 
 gulp.task('style', function () {
   gulp.src('sass/style.scss')
@@ -27,27 +29,54 @@ gulp.task('style', function () {
         sort: true
       })
     ]))
-    .pipe(gulp.dest('css'))
+    .pipe(gulp.dest('build/css'))
     .pipe(minify())
     .pipe(rename("style.min.css"))
-    .pipe(gulp.dest('css'))
+    .pipe(gulp.dest('build/css'))
     .pipe(server.reload({ stream: true }));
-})
+});
 
 gulp.task('images', function () {
-  return gulp.src('img/**/*.{png,jpg}')
+  return gulp.src('build/img/**/*.{png,jpg}')
     .pipe(imagemin([
-      imagemin.optipng({optimizationLevel: 3}),
-      imagemin.jpegtran({progressive: true})
+      imagemin.optipng({ optimizationLevel: 3 }),
+      imagemin.jpegtran({ progressive: true })
     ]))
     .pipe(gulp.dest('build/img'));
-})
+});
 
-gulp.task('server', ['style'], function () {
+gulp.task('clean', function () {
+  return del("build");
+});
+
+gulp.task('copy', function () {
+  return gulp.src([
+    'fonts/**/*.{woff,woff2}',
+    'img/**',
+    'js/**',
+    '*.html'
+  ], {
+      base: "."
+    })
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('build', function (fn) {
+  run(
+    'clean',
+    'copy',
+    'style',
+    'images',
+    fn
+  );
+});
+
+
+gulp.task('server', function () {
   server.init({
-    server: "."
+    server: "build"
   });
-
   gulp.watch('sass/**/*.scss', ['style']);
   gulp.watch('*.html').on('change', server.reload);
 });
+
